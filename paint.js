3e8e1da695161baa0290d7f7ea8e5826977e04f8
Tom Nguyen <huuthong103@gmail.com>
2019-12-27 15:49:23 -0800

    finished the draw app

--- paint.js
+++ paint.js
@@ -5,6 +5,7 @@ class paint{
         this.canvas.height = 500;
         this.ctx = this.canvas.getContext("2d");
         this.drawBackground();
+        
         this.color = '#ff0000';
         this.tool = 'pen';
         this.lineWidth = 5;
@@ -12,16 +13,23 @@ class paint{
         this.currentPos = {
             x:0,
             y:0
-        }
+        };
 
         this.drawing = false;
 
-        this.image = null;
+        this.oldImage = null;
+        this.newImage = null;
+
+        this.startPos = {
+            x:0,
+            y:0
+        }
 
         // listen mouse event
         this.listenEvent();
         this.drawLine(10,10,100,100);
     }
+
     getMousePos(evt){
         var rect = this.canvas.getBoundingClientRect();
         return{
@@ -32,16 +40,34 @@ class paint{
 
     mousedown(event){
         // console.log("mouse down");
-        this.image = new Image;
-        this.image.src = this.canvas.toDataURL("image/bmp", 1.0);
+        this.saveState();
 
         let mousePos = this.getMousePos(event);
+        this.startPos = this.getMousePos(event);
+        
         this.drawing = true;
     }
     mousemove(event) {
         let mousePos = this.getMousePos(event);
         if(this.drawing){
-            this.drawLine(this.currentPos, mousePos);
+            switch(this.tool){
+                case 'pen':
+                    this.drawLine(this.currentPos,mousePos)
+                break;
+                case 'line':
+                    this.undo();
+                    this.drawLine(this.startPos, mousePos)
+                break;
+                case 'rect':
+                    this.undo();
+                    this.drawRect(this.startPos, mousePos)
+                break;
+                case 'rect':
+                    this.undo();
+                    this.drawRect(this.startPos, mousePos)
+                break;
+            }
+            // this.drawLine(this.currentPos, mousePos);
         }
         this.currentPos = mousePos;
     }
@@ -59,12 +85,35 @@ class paint{
     }
 
     undo(){
-        this.ctx.drawImage(this.image, 0,0,800,500);
+        this.newImage = new Image;
+        this.newImage.src = this.canvas.toDataURL("image/bmp", 1.0);
+
+        this.ctx.drawImage(this.oldImage, 0,0,800,500);
+    }
+
+    saveState(){
+        this.oldImage = new Image;
+        this.oldImage.src = this.canvas.toDataURL("image/bmp", 1.0);
+    }
+
+    redo(){
+        this.ctx.drawImage(this.newImage, 0, 0, 800, 500);
     }
 
     drawBackground(){
         this.ctx.fillStyle = '#ffffff';
         this.ctx.fillRect(0,0,800,500);
+    }
+
+    drawRect(startPos, endPos){
+        this.ctx.beginPath();
+        this.ctx.rect(
+            startPos.x, 
+            startPos.y, 
+            endPos.x - startPos.x,
+            endPos.y - startPos.y
+        );
+        this.ctx.stroke();
     }
 
     drawLine(startPos, endPos){
